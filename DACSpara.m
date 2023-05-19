8f83a8673262379935cd824c24eea08b066f531b
TNTTNTTTN <isugkgkgk@gmail.com>
2023-05-19 17:56:24 +0900

    TPNG modified

--- DACSpara.m
+++ DACSpara.m
@@ -22,8 +22,8 @@ Amax = 362.97; % Max attitude thrust [N]
 
 % Initial condition
 phi = 0; % degree
-theta = 25; % degree
-psi = 25; % degree
+theta = 30; % degree
+%psi = 0; % degree
 UVWinit = [1000; 0; 0]; % Initial velocity of Missile [m/s;m/s;m/s]
 PQRinit = [0; 0; 0]; % Initial anglar rate of Missile [rad/s;rad/s;rad/s]
 POSinit = [0; 0; 0]; % Initial Position of Missile [m;m;m];
@@ -44,12 +44,15 @@ K2 = 200;
 % Gain for PPNG
 N = 5;
 
+% Gain for TPNG
+Nt = 5;
 % Gain for PG
 Kr = 5;
 
 % Target Initial parameter
 LoS = Euler2Quat([0;30;10]*d2r);
 Tpos = Quatmul(Quatmul(LoS,[1;0;0]),Quatinv(LoS));
-TPosition = 20000*[Tpos(2);Tpos(3);-Tpos(4)]; % Initial Position of Target [m;m;m]
+Tdist = 15000;
+TPosition = Tdist*[Tpos(2);Tpos(3);-Tpos(4)]; % Initial Position of Target [m;m;m]
 Theading = Euler2Quat([0;-30;180].*d2r); % Heading angle of Target
 Tspeed = 2500;
--- plotting.m
+++ plotting.m
@@ -1,17 +1,35 @@
-h1 = animatedline('Color', 'r', 'LineWidth', 3);
-h2 = animatedline('Color', 'b', 'LineWidth', 3);
 hold on
 grid on
 xlabel("North");
 ylabel("East");
 zlabel("Height");
-daspect([1 1 1])
-view(3)
-for k = 1:length(tout)
-    addpoints(h1, TPOS.Data(1,k), TPOS.Data(2,k), TPOS.Data(3,k))
-    addpoints(h2, POSITION.Data(k,1), POSITION.Data(k,2), POSITION.Data(k,3))
-    drawnow
+h1 = animatedline('Color', [0.4940 0.1840 0.5560], 'LineWidth', 3,'LineStyle' ,'--');
+for i = 1:3
+    if i==1
+        h2 = animatedline('Color', [0 0.4470 0.7410], 'LineWidth', 3);
+        %theta = 25;
+        psi = 25; % degree
+    elseif i==2
+        h2 = animatedline('Color', [0.9290 0.6940 0.1250], 'LineWidth', 3);
+        %theta = 30;
+        psi = 30; % degree
+    else
+        h2 = animatedline('Color', [0.8500 0.3250 0.0980], 'LineWidth', 3);
+        %theta = 35;
+        psi = 35; % degree
+    end
+    sim("DACS")
+    daspect([1 1 1])
+    view(3)
+    for k = 1:length(tout)
+        addpoints(h1, TPOS.Data(1,k), TPOS.Data(2,k), TPOS.Data(3,k))
+        addpoints(h2, POSITION.Data(k,1), POSITION.Data(k,2), POSITION.Data(k,3))
+        drawnow
+    end
+    disp(abs(norm(TPOS.Data(:,end-2)) - norm(POSITION.Data(end-2,:))));
 end
+
+
 %plot3(TPOS.data(1,:),TPOS.data(2,:),TPOS.data(3,:), '-r', 'LineWidth', 4);
 %daspect([1 1 1])
 %hold on
@@ -20,5 +38,5 @@ end
 %ylabel("East");
 %zlabel("Height");
 %plot3(POSITION.Data(:,1), POSITION.Data(:,2), POSITION.Data(:,3), '-b', 'LineWidth', 4);
-disp(abs(norm(TPOS.Data(:,end-2)) - norm(POSITION.Data(end-2,:))));
+
 
